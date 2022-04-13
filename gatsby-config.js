73076d9f516c35e50b393045d94271a036c5e6de
Rohan Nagavardhan <rnagavar@umich.edu>
2022-04-13 09:03:50 -0400

    Updates

--- gatsby-config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  siteMetadata: {
-    title: `reshape-landing`,
-    siteUrl: `https://www.yourdomain.tld`,
-  },
-  plugins: [],
-}
--- secrets.js
+++
@@ -1,7 +0,0 @@
-const keys = {
-    apiKey: "key32y0lUm43jwmU1",
-    baseId: "appgG7EJWvlt77ShS",
-    tableId: "tbl6Swd9LM0GQEXaM"
-}
-
-export default keys;
